2b5f41faa4d3f8b4a8e72b6190e187bed93c5500
JVictor011 <jvictor2021@alu.ufc.br>
2024-02-21 02:47:15 -0300

    new functions for auth

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,6 +3,8 @@ const { Sequelize, Op } = require("sequelize");
 const session = require("express-session");
 const passport = require("passport");
 const authRoutes = require("../OAuth/githubOAuth");
+const User = require("../database/models/user");
+const session = require("express-session");
 require("../OAuth/googleOAuth");
 
 exports.githubAuth = passport.authenticate("github", { scope: ["user:email"] });
@@ -44,5 +46,55 @@ exports.checkAuthAndRedirect = (req, res) => {
     res.redirect("/home");
   } else {
     res.render("login.ejs", { user: req.user });
+  }
+};
+
+exports.register = async (req, res) => {
+  try {
+    const { username, password, email } = req.body;
+
+    await User.create({
+      username,
+      password,
+      email,
+    });
+
+    res.redirect("/login");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro interno no servidor");
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ raw: true, where: { username } });
+
+    if (user && user.validPassword(password)) {
+      req.session.username = username;
+      res.redirect("/home");
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro interno no servidor");
+  }
+};
+
+exports.logoutUserForm = (req, res) => {
+  try {
+    req.session.destroy((error) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send("Erro ao encerrar a sessão");
+      } else {
+        res.redirect("/");
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro interno no servidor");
   }
 };
